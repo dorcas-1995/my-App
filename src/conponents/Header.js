30f4508584e4d6d15ca6692a19f81b5982c1155a
dorcas-1995 <wayumulu58@gmail.com>
2022-07-28 00:26:46 -0400

    Header styling commit

--- src/conponents/Header.js
+++ src/conponents/Header.js
@@ -4,7 +4,7 @@
      return (
        <div>
         <header>
-            <h1>{title}</h1>
+            <h1 style={{ color: 'black', bgcolor: 'blur'}}>{title}</h1>
             {/* <h1 className="bg-success ">Dogs Breeds</h1> */}
         </header>
 
