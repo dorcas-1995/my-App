8cd482567b6e42a36aa16356b7e60dc81ea8da63
dorcas-1995 <wayumulu58@gmail.com>
2022-07-28 11:44:43 -0400

    react-router-dom commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Header from './conponents/Header'
 import Navbar from './conponents/Navbar'
+import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div className="container">
     <Header/>
     <Navbar/>
      
     </div>
+    </Router>
   );
 }
 
--- src/conponents/Navbar.js
+++ src/conponents/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="navbar bg-light">
+        <ul className=''navbar-nav>
+            <li>
+                <Link to="/">HOME</Link>
+            </li>
+            <li>
+                <Link to="/search">SEARCH</Link>
+            </li>
+
+        </ul>
     </nav>
   )
 }
