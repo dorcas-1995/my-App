5f5fc360ba22a283276a2a097886978ecddda40e
dorcas-1995 <wayumulu58@gmail.com>
2022-07-28 00:14:53 -0400

    passing props title commit

--- src/conponents/Header.js
+++ src/conponents/Header.js
@@ -1,14 +1,19 @@
    //import React from 'react'
    
- const Header = () => {
+ const Header = ({title}) => {
      return (
        <div>
         <header>
-            <h1 className="bg-success">Dogs Breeds</h1>
+            <h1>{title}</h1>
+            {/* <h1 className="bg-success ">Dogs Breeds</h1> */}
         </header>
 
        </div>
      )
+   }
+
+   Header.defaultProps ={
+    title: 'Dogs Breeds',
    }
 
    export default Header
