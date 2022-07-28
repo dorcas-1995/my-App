c50e2384477c94bc160a63a35805fef1166b9eb2
dorcas-1995 <wayumulu58@gmail.com>
2022-07-28 00:04:25 -0400

    adding Header commit

---
+++ src/conponents/Header.js
@@ -0,0 +1,15 @@
+   //import React from 'react'
+   
+ const Header = () => {
+     return (
+       <div>
+        <header>
+            <h1 className="bg-success">Dogs Breeds</h1>
+        </header>
+
+       </div>
+     )
+   }
+
+   export default Header
+   
